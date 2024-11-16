075cc10bb0c02cf714c8867989139000bc1f4798
tphiri023 <tphiri023@student.wethinkcode.co.za>
2024-11-16 14:23:36 +0200

    Created a dummy test

---
+++ tests/test_fact_generator.py
@@ -0,0 +1,8 @@
+import unittest
+
+from src.fact_generator import fetch_fact,  fetch_multiple_facts
+class TestFactGenerator(unittest.TestCase):
+    def test_something(self):
+        self.assertFalse(False)
+if __name__ == '__main__':
+    unittest.main()
